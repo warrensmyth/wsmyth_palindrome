3211e429831f761e839c0b93f107370bf3375f46
Warren Smyth <warrensmyth@me.com>
2020-12-17 14:34:50 -0600

    Add letters method and get tests to green

--- lib/wsmyth_palindrome.rb
+++ lib/wsmyth_palindrome.rb
@@ -1,6 +1,27 @@
 require "wsmyth_palindrome/version"
 
-module WsmythPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String 
+
+	# Returns true for a palindrome, false otherwise.
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
+
+	# Returns the letters in a string
+	def letters
+		the_letters = []
+		for i in 0..self.length - 1 do
+			if self[i].match(/[a-zA-z]/)
+				the_letters << self[i]
+			end
+		end
+		the_letters.join
+	end
+
+	private
+
+		# Returns content for palindrome testing
+		def processed_content
+			self.letters.to_s.downcase
+	end
 end
--- test/wsmyth_palindrome_test.rb
+++ test/wsmyth_palindrome_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class WsmythPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::WsmythPalindrome::VERSION
+  
+  def test_non_palindrome
+  	refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_literal_palindrome
+  	assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+  	assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+  	assert "Madam, I'm Adam".palindrome?
+  end
+
+  def test_letters
+  	assert_equal "MadamImAdam", "Madam, I'm Adam".letters
   end
 end
