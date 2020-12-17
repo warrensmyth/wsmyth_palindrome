7ed7e619b3ce400ddbcb790805a0ac0b82ad8eaf
Warren Smyth <warrensmyth@me.com>
2020-12-17 16:07:03 -0600

    Change integer_non_palindrome test to better match ruby convention of using refute

--- test/wsmyth_palindrome_test.rb
+++ test/wsmyth_palindrome_test.rb
@@ -19,7 +19,7 @@ class WsmythPalindromeTest < Minitest::Test
   end
 
   def test_integer_non_palindrome
-  	assert !12345.palindrome?
+  	refute 12345.palindrome?
   end
 
   def test_integer_palindrome
